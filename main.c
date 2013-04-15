62f670d51d8a2b948fa2beb0b99c818feaf3f60c
dialogwtf <romak50@gmail.com>
2013-04-15 19:56:04 +0400

    my first commit

--- main.c
+++ main.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#define input_buf_size 80
+#define SIZE( x ) (sizeof( x )/sizeof( *x ))
 
-int main()
+char *gets_s(char *s, size_t buf_size)
 {
-    return 0;
+    char *result;
+    result = fgets(s, buf_size, stdin);
+    result[strlen(s) - 1] = '\0';
+    return result;
 }
+
+typedef struct student {
+	char name[100];
+	char surname[100];
+	char facaul[100];
+	char spec[100];
+	int group[50];
+	int marks[50];
+} stud;
