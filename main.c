7689ba705d50ce4a0dec561e198582fef23e9ee9
Admin <dialog@ubuntu.ubuntu-domain>
2013-04-17 22:09:13 +0300

    fun menu

--- main.c
+++ main.c
@@ -13,10 +13,41 @@ char *gets_s(char *s, size_t buf_size)
 }
 
 typedef struct student {
-	char name[100];
-	char surname[100];
-	char facaul[100];
-	char spec[100];
-	int group[50];
-	int marks[50];
+    char name[100];
+    char surname[100];
+    char facaul[100];
+    char spec[100];
+    int group[50];
+    int marks[50];
 } stud;
+
+int prog_menu(void)
+{
+    char s[input_buf_size];
+    int N;
+    
+
+    printf("=> 1 <= Enter student data");
+    printf("=> 2 <= Look primed student");
+    printf("=> 3 <= Exit");
+    do {
+        printf("Enter your choise");
+        gets_s(s, input_buf_size);
+        c=atoi(s);
+    }
+    while(0 > c || c > 3)
+    return c;
+
+
+}
+
+
+int main()
+{
+
+
+
+
+}
+
+end.
